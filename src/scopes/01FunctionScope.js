686b82ff8192e340de31b4fcd1b2338331e662af
ngaikwad3 <ngaikwad3@dxc.com>
2022-12-25 13:12:23 +0530

    Basic and Scopes realted examples

---
+++ src/scopes/01FunctionScope.js
@@ -0,0 +1,11 @@
+var name = "Nitin"
+
+function allocateCity() {
+    if (name == "Nitin") {
+        var city = "Indore";
+    }
+
+}
+
+allocateCity();
+//console.log(city); // Runtime Error ReferenceError: city is not defined
---
+++ src/scopes/02IIFE.js
@@ -0,0 +1,9 @@
+// Imidiatly Invode Function Expression 
+
+(
+function () {
+    var a =10;
+    var b=20;
+    console.log(a+b);
+}
+)();
